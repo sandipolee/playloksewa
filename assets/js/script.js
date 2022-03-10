b94fa62ea1fd5d8663bce6a7e05da0076458684d
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2022-03-10 08:34:33 +0545

    toggle button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 function toggle() {
-    alert("toggle")
+    let nav = document.querySelector("#nav-bar")
+    if (nav.classList.contains("nav-toggle")) {
+        nav.classList.replace("nav-toggle", "nav-bar")
+    } else
+        nav.classList.replace("nav-bar", "nav-toggle")
 }
