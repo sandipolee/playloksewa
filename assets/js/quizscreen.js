e814d7de1a4435fae1e0159f01b82c950872fb8c
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2022-03-12 08:40:36 +0545

    new

---
+++ assets/js/quizscreen.js
@@ -0,0 +1,11 @@
+let currentQuestion = 0;
+let score = 0;
+let correctans_count = 0;
+let Wrongans_count = 0;
+
+function playquiz(cIndrex) {
+    let quiz_bg = document.querySelector("#quiz-bg");
+
+    quiz_bg.classList.add("target")
+
+}
