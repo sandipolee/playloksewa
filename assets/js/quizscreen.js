8f8b2ec4bb69152590709ec8bdc9eb0b100406b2
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2022-03-12 09:52:13 +0545

    hhh

--- assets/js/quizscreen.js
+++ assets/js/quizscreen.js
@@ -2,10 +2,3 @@ let currentQuestion = 0;
 let score = 0;
 let correctans_count = 0;
 let Wrongans_count = 0;
-
-function playquiz(cIndrex) {
-    let quiz_bg = document.querySelector("#quiz-bg");
-
-    quiz_bg.classList.add("target")
-
-}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
+document.onload(alert(loading))
+
 function toggle() {
     let nav = document.querySelector("#nav-bar")
     if (nav.classList.contains("nav-toggle")) {
         nav.classList.replace("nav-toggle", "nav-bar")
     } else
         nav.classList.replace("nav-bar", "nav-toggle")
+}
+
+function playquiz(cIndrex) {
+    sessionStorage.setItem("cIndex", cIndrex);
+    location.href = "./play.html";
 }
