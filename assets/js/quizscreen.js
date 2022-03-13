618ee8996950763d61cd743156dcd93db4f84518
Sandip Olee <52390136+sandipolee@users.noreply.github.com>
2022-03-13 19:40:16 +0545

    ggjhghjg

--- assets/js/quizscreen.js
+++ assets/js/quizscreen.js
@@ -24,6 +24,7 @@ function pad(val) {
 
 let currentQuestion = 0;
 let score = 0;
+let question_count = 0;
 let correctans_count = 0;
 let Wrongans_count = 0;
 
@@ -66,4 +67,44 @@ function getrandom(max) {
 
 function getquestion() {
     currentQuestion = getrandom(4);
+}
+
+function getnewque() {
+    let increaser = getrandom(4);
+    if (increaser === 0) {
+        getnewque();
+
+    } else {
+        currentQuestion = currentQuestion + increaser;
+    }
+
+}
+
+function nextbtn(qdata) {
+
+    getnewque()
+    let tag_Data = qdata[tag]["data"];
+    let ans = tag_Data[currentQuestion]["answers"];
+
+    let question = document.querySelector("#qesdiv");
+    let ans_ul = document.querySelector("#ans-area");
+
+
+
+    question.innerHTML = tag_Data[currentQuestion]["question"];
+    ans_ul.innerHTML = `<li class="optionQ" id="0">${ans[0]} </li>
+    <li class="optionQ" id="1">${ans[1]} </li>
+    <li class="optionQ" id="2">${ans[2]} </li>
+    <li class="optionQ" id="3">${ans[3]} </li>`
+}
+
+function next() {
+
+    fetch(quiz_data)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            nextbtn(data);
+        });
 }
